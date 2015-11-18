250df66ea101abbbf6e60e298c9592cda340ba51
bryan hunt <github@binarytemple.co.uk>
2015-11-18 18:47:01 +0000

    generate list of partitions

--- src/riak_canary_generator.erl
+++ src/riak_canary_generator.erl
@@ -10,24 +10,24 @@
 -author("bryanhunt").
 
 %% API
--export([calc/1,randomGen/0]).
+-export([calc/1, randomGen/0, gen_partitions/1]).
 
 -spec randomGen() -> string().
 randomGen() ->
   integer_to_list(round(random:uniform() * 10000000000000000000000000000000))
 .
 
-
 -spec calc(integer()) -> list().
 calc(RingSize) ->
 calc(RingSize, fun riak_canary_generator:randomGen/0)
 .
 
-
 -spec calc(integer(), fun()) -> list().
 calc(_,RGFunc)->
-
 RGFunc()
-
 .
+
+-spec gen_partitions(integer()) -> list(integer()).
+gen_partitions(RingSize) when is_integer(RingSize) ->
+[  ((trunc(math:pow(2,160)-1) div RingSize) * Res  )|| Res <- lists:seq(0,RingSize)] .
 
